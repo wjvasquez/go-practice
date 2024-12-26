bdf150d280325225b650244ca0a9570ffabaad0b
Wjvasquez <wilmanvasdez@gmail.com>
2024-12-26 09:12:04 -0500

    Add feedback to guess the number app

--- guess_the_number/main.go
+++ guess_the_number/main.go
@@ -5,9 +5,12 @@ import (
 	"math/rand"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
 	var maxAttempts int
 	var initialInput string
 	randomNumber := rand.Intn(100) + 1
@@ -16,6 +19,11 @@ func main() {
 	fmt.Printf("\nI'm thinking of a number between 1 and 100. Can you guess it?\n")
 	fmt.Print("How many attempts do you want to get? ")
 	fmt.Scanln(&maxAttempts)
+
+	if maxAttempts <= 0 {
+		fmt.Println("Please enter a positive number of attempts.")
+		return
+	}
 
 	for {
 		fmt.Print("Enter your guess: ")
@@ -35,17 +43,21 @@ func main() {
 
 		attempts++
 
+		if guessNumber < 1 || guessNumber > 100 {
+			fmt.Println("Please guess a number between 1 and 100.")
+		}
+
 		if guessNumber == randomNumber {
 			fmt.Printf("\nCorrect! You've guessed the number in %d attempts.\n", attempts)
 			break
 		} else if guessNumber < randomNumber {
-			fmt.Println("Too low! Try again.")
+			fmt.Printf("Too low! Try again. You have %d attempts left.\n", maxAttempts-attempts)
 		} else {
-			fmt.Println("Too high! Try again.")
+			fmt.Printf("Too high! Try again. Yuo have %d attempts left.\n", maxAttempts-attempts)
 		}
 
 		if attempts == maxAttempts {
-			fmt.Printf("\nYou've reached the maximum number of attemps.\n")
+			fmt.Printf("\nYou've reached the maximum number of attempts.\n")
 			fmt.Printf("The number was %d.\n", randomNumber)
 			break
 		}
